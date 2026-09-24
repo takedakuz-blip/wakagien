8da081de489ee6cb0978bee241daba0eb2f18282
takedakuz-blip <takedakuz@gmail.com>
2026-09-24 13:51:37 +0900

    Add files via upload

---
+++ README.txt
@@ -0,0 +1,6 @@
+若木園 iPhone版
+
+このフォルダ一式をHTTPS対応のWebホスティングへ配置してください。
+Safariでindex.htmlのURLを開き、共有 > ホーム画面に追加 で利用できます。
+園地図 wakagien-map.png は同梱済みです。
+バックアップボタンでJSON保存、復元で戻せます。
---
+++ service-worker.js
@@ -0,0 +1,1 @@
+const C='wakagien-v1',F=['./','./index.html','./wakagien-map.png','./manifest.webmanifest','./icon-180.png','./icon-192.png','./icon-512.png'];self.addEventListener('install',e=>e.waitUntil(caches.open(C).then(c=>c.addAll(F))));self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
